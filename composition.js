f613368eae018dc5a715a0e71f8876175a96ea40
Dasha <davydova_di@mail.ru>
2020-08-08 17:57:38 +0300

    feat: compozition numbers|composition.js

---
+++ composition.js
@@ -0,0 +1,14 @@
+const num = 266219;
+
+const space = '';
+let comp = 1;
+numArr = num.toString().split(space);
+
+console.log(numArr);
+
+for (let i = 0; i < numArr.length; i++) {
+    comp *= numArr[i];
+}
+
+console.log('Произведение цифр числа равно:', comp);
+console.log('Первые две цифры числа:', numArr[0],numArr[1],);
