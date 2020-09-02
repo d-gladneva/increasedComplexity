160d1c99567b879f4a43434557d5570b024f32fb
Dasha <davydova_di@mail.ru>
2020-09-02 12:28:43 +0300

    fix: function week|script.js

--- lesson07/weekArr.js
+++ lesson07/weekArr.js
@@ -1,3 +1,23 @@
+//
+// let week = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вск'];
+//
+// // toDay = new Date().getDay() - 1;
+//
+// const addWeek = function (week) {
+//
+//     const arrWeek = document.querySelector('#arr_week');
+//
+//     week.forEach((el) => {
+//         arrWeek.insertAdjacentHTML('beforeEnd',  `<li class="cell">${el}</li>`);
+//     });
+// };
+//
+// addWeek(week);
+//
+//
+//
+//
+
 let week = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вск'];
 
 toDay = new Date().getDay() - 1;
@@ -5,17 +25,19 @@ toDay = new Date().getDay() - 1;
 const addWeek = function (week) {
 
     for (let i = 0; i < 7; i++) {
-
         if (i === toDay) {
             if (week[i] === 'сб' || week[i] === 'вск') {
                 document.write(`<p><b>${week[i]}</b></p>`);
-            } else document.write(`<p><i>${week[i]}</i></p>`);
+            } else {
+                document.write(`<p><i>${week[i]}</i></p>`);
+            }
         } else if (week[i] === 'сб' || week[i] === 'вск') {
             document.write(`<p><b>${week[i]}</b></p>`);
         } else {
             document.write(`<p><i>${week[i]}</i></p>`);
         }
     }
+
 };
 
 addWeek(week);
