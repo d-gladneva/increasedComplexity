1a02ddf435e1e086a4bf6562b2e9ea720dc63596
Dasha <davydova_di@mail.ru>
2020-09-02 13:54:45 +0300

    fix: function checkPrime

--- lesson05/createNum.js
+++ lesson05/createNum.js
@@ -1,6 +1,6 @@
 const arrNum = [];
 
-for (let i=0; i<3; i++){
+for (let i=0; i<7; i++){
     const num = prompt('Введите многозначное число');
     arrNum.push(num);
     if (String(arrNum[i]).substring(0,1)=== '2' || String(arrNum[i]).substring(0,1)=== '4')
@@ -9,23 +9,21 @@ for (let i=0; i<3; i++){
 
 console.log(arrNum);
 
-const divider = [];
 
 
-const checkPrimeNum = function() {
-    for (let i=2; i < 10; i++) {
+const checkPrimeNum = function () {
+    for (let i = 2; i <= 10; i++) {
         let primeNum = true;
-        for (let j = 2; (j < i/2)&&(primeNum = true); j++) {
-            if (i%j===0) {
+        for (let j = 2; j <= i; j++) {
+            if (i % j === 0 && i !== j) {
                 primeNum = false;
                 break;
+            } else {
+                primeNum = true;
             }
         }
-        let n=1;
-        if (primeNum === true){
-            if (i%n===0) {
-                console.log(i + ' Делители этого числа: ' + n);
-            }
+        if (primeNum) {
+            console.log(i + ' Делители этого числа: ' + 1 + ';'+ i);
         }
     }
 };
